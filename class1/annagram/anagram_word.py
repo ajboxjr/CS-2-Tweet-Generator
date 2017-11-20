a5b84c20f0640d2fc056ad68a9d50b0d346e6fc4
Anthony Box <ajboxjr@gmail.com>
2017-11-19 23:05:23 -0800

    generate possible annagram words

--- class1/annagram/anagram_word.py
+++ class1/annagram/anagram_word.py
@@ -1,30 +1,49 @@
 import sys
+from itertools import permutations
 import random
 
-def get_pos_words(annagram_word):
-    pos_word_arr = []
-    annagram_set = set(annagram_word)
-    print(annagram_set)
-    with open('/usr/share/dict/words', 'r') as f:
-        dictionary = f.read().split()
-        for word in dictionary:
-            if len(word) <= len(annagram_word):
-                for char in annagram_word:
+class Annagram(object):
+    def __init__(self,word):
+        self.word = None
+        if word:
+            self.word = word
+            self.get_pos_words()
+
+    def set_word(self,word):
+        """Set Annagram Word"""
+        self.word = word
+    def has_word(self):
+        """Check if class has a word for annagrams"""
+        return self.word != None
+    
+
+    def get_pos_words(self):
+        pos_word_arr = []
+        annagram_set = set(self.word)
+        for word in get_dictionary():
+            if len(word) <= len(self.word):
+                for char in annagram_set:
                     if word.startswith(char):
                         pos_word_arr.append(word)
-    return pos_word_arr
+        return pos_word_arr
 
-def word_combinations(user_word, pos_word= None):
-    compare_set = []
-    for char in pos_word:
-        word = set(user_word.remove(char))
-        set
-        compare_set.append(pos_word.(word))
-    print(compare_set)
-        
+            
+    def word_permutation(self):
+        pos_words = self.get_pos_words()
+        print(pos_words)
+        for length in range(1,len(self.word)+1):
+            perms = set(map("".join, permutations(self.word, length)))
+            for word in perms:
+                if word in pos_words:
+                    print(word)
+
+def get_dictionary():
+    with open('/usr/share/dict/words', 'r') as f:
+        dictionary = f.read().split()
+    return dictionary
 
 if __name__ == '__main__':
     input_word = sys.argv[1]
-    pos_arr = get_pos_words(input_word)
-    word_combination(input_word)
+    pos_arr = Annagram(input_word)
+    pos_arr.word_permutation()
 
